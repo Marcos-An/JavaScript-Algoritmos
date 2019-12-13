3879a2c2f1dbe8616679428c68f326fd099d3822
Marcos-An <loseonjr@gmail.com>
2019-12-13 08:59:03 -0300

    List-sort

---
+++ List-Booble.js
@@ -0,0 +1,85 @@
+function ListaLigada() {
+  let cabeca
+  let tamanho = 0
+
+  const Print = () => {
+    console.log(cabeca)
+  }
+
+  const Node = (value) => {
+    let valor = value
+    let prox = null
+    return {
+      valor,
+      prox
+    }
+  }
+
+  const order = () => {
+    if (tamanho > 1) {
+      for (var i = 0; i <= tamanho; i++) {
+        let node = cabeca
+        let next = cabeca.prox
+        for (let j = 0; j < tamanho - 1; j++) {
+          if (node.valor > next.valor) {
+            let aux = node.valor
+            node.valor = next.valor
+            next.valor = aux
+          }
+          node = next
+          next = next.prox
+        }
+      }
+    }
+    return cabeca
+  }
+
+  const SelectById = (index) => {
+    if (tamanho === 0) return null
+    let node = cabeca
+    let count = 0
+
+    while (count < index) {
+      node = node.prox
+      count++
+    }
+    return node
+  }
+
+  const Adicionar = (value) => {
+    if (tamanho === 0) {
+      cabeca = Node(value)
+      tamanho++
+      return cabeca
+    }
+    let node = cabeca
+    while (node.prox) {
+      node = node.prox
+    }
+    node.prox = Node(value)
+    tamanho++
+    return node
+  }
+
+  return {
+    Print: () => Print(),
+    add: (value) => Adicionar(value),
+    SelectById: (index) => SelectById(index),
+    order: () => order()
+  }
+}
+
+Lista = ListaLigada()
+Lista.add(6)
+Lista.add(5)
+Lista.add(2)
+Lista.add(3)
+Lista.add(9)
+Lista.add(16)
+Lista.add(4)
+Lista.add(1)
+console.log(Lista.Print());
+console.log(Lista.order());
+
+
+
---
+++ Listed-sort.js
@@ -0,0 +1,100 @@
+function linkedList() {
+  let head
+  let length = 0
+
+  const Print = () => {
+    let node = head
+    let AHead = []
+    while (node.next) {
+      AHead.push(node.name)
+      node = node.next
+    }
+    console.log(AHead);
+  }
+
+  const Node = (value) => {
+    let name = value
+    next = null
+    return { name, next }
+  }
+
+  const Sort = () => {
+    if (length > 1) {
+      for (let i = 0; i < length; i++) {
+        let node = head
+        let next = head.next
+        for (let j = 0; j < length - 1; j++) {
+          if (node.name > next.name) {
+            let aux = node.name
+            node.name = next.name
+            next.name = aux
+          }
+          node = next
+          next = next.next
+        }
+      }
+    }
+    return head
+  }
+  const selectionPerson = () => {
+    let node = head
+    let count = 0
+    let goTo = Math.floor(Math.random() * (length - 1))
+    while (count < goTo) {
+      node = node.next
+      count++
+    }
+    return node.name
+  }
+
+  const selection = (qty) => {
+    if (length === 0) return null
+    let node
+    let node2
+    let i = 0
+    while (i <= qty) {
+      node = selectionPerson()
+      node2 = Add(node)
+      i++
+    }
+    return console.log(node2);
+  }
+
+  const Add = (value) => {
+    if (length === 0) {
+      head = Node(value)
+      length++
+      return head
+    }
+
+    let node = head
+    while (node.next) {
+      node = node.next
+    }
+    node.next = Node(value)
+    length++
+    return node
+  }
+
+  return {
+    Add: (value) => Add(value),
+    Order: () => Sort(),
+    Print: () => Print(),
+    selection: (qty) => selection(qty)
+  }
+}
+
+List = linkedList()
+List.Add("Marcos")
+List.Add("Fernanda")
+List.Add("Tarciso")
+List.Add("Matheus Alves")
+List.Add("Matheus Nogueira")
+List.Add("Carol")
+List.Add("Kati")
+List.Add("Yoshio")
+List.Add("Julia")
+List.Order()
+List.Print()
+console.log("");
+List.selection()
